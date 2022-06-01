c4eaa3ee8c0cb4a22c3f1591f2b77cfcf4f15c8a
NikolayMG1 <99046600+NikolayMG1@users.noreply.github.com>
2022-06-02 01:26:07 +0300

    Delete Moon.cpp

--- Moon.cpp
+++
@@ -1,9 +0,0 @@
-#include "Moon.h"
-
-Moon::Moon(){
-    
-}
-    Moon(const char* nameMoon,const Jedi* jedi,const char* name, Ranks rank, unsigned age, char* saberColor, double strength);
-    Moon(const Moon&);
-    Moon& operator=(const Moon&);
-    ~Moon();
